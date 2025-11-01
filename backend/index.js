5d9ef7c391a078cec74a8fbe7ab0cab03ff8b2fc
Aadarsh Kumar <your_email@example.com>
2025-11-01 07:59:46 +0530

    Fix: corrected DB import/export syntax

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./config/db.js";
+import { connectDB } from "./config/db.js";
 import adminRoutes from "./routes/admin.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
